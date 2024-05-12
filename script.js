b77ee2a70c8221a5e2e7a1d2a19fb4a745379ec4
bartoszduch <139812577+bartoszduch@users.noreply.github.com>
2024-05-12 12:16:07 +0200

    commit

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const useLocationBtn = document.getElementById('use-location');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevents the form's default action
+        event.preventDefault(); // zapobiega domyślnym zmiana
         submitCoordinates();
     });
 
-    useLocationBtn.addEventListener('click', function() {
+    useLocationBtn.addEventListener('click', function() { //niestety na wczytanie danych na podstawie lokalizacji trzeba chwilę poczekać, aplikacja niesety nie odpowiada tak szybko jak w przypadku wpisania współżędnych z klawiatury
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 document.getElementById('latitude').value = position.coords.latitude.toFixed(2);
                 document.getElementById('longitude').value = position.coords.longitude.toFixed(2);
-                submitCoordinates(); // Automatically submit after filling coordinates
+                submitCoordinates(); // aktualizacja po zmianie współżędnych 
             }, function(error) {
                 alert('Error: ' + error.message);
             });
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (!isValidCoordinates(latitude, longitude)) {
             alert('Please enter valid latitude (between -90 and 90) and longitude (between -180 and 180) values.');
-            return; // Stop the function if validation fails
+            return; 
         }
         const url = `http://127.0.0.1:5000/weather?latitude=${latitude}&longitude=${longitude}`;
 
@@ -51,12 +51,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const thead = table.tHead;
         thead.innerHTML = '';
         const headerRow = document.createElement('tr');
-        // Dodajemy tylko jeden nagłówek z zazwą date
+
         const dateHeader = document.createElement('th');
         dateHeader.textContent = "Date";
         headerRow.appendChild(dateHeader);
 
-        // Dodajemy nagłówki dla każdego dnia
+        
         data.forEach((dayData) => {
             const dateHeader = document.createElement('th');
             dateHeader.textContent = dayData.date;
@@ -72,6 +72,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const row = document.createElement('tr');
             const fieldName = document.createElement('td');
             fieldName.textContent = field.replace('_', ' ').toUpperCase();
+            fieldName.textContent = field.replace('energy', 'Generated energy [kwh]').toUpperCase();
             row.appendChild(fieldName);
             tbody.appendChild(row);
     
@@ -92,39 +93,39 @@ document.addEventListener('DOMContentLoaded', function() {
     
 });
 
-// Helper function to map weather codes to Font Awesome icons
+// funkcja która mapuje kody pogody na ikony
 function weatherCodeToIcon(code) {
     const map = {
-        '0': 'fa-sun',                 // Clear sky
-        '1': 'fa-cloud-sun',           // Mainly clear
-        '2': 'fa-cloud-sun',           // Partly cloudy
-        '3': 'fa-cloud',               // Overcast
-        '45': 'fa-smog',               // Fog
-        '48': 'fa-smog',               // Depositing rime fog
-        '51': 'fa-cloud-rain',         // Light drizzle
-        '53': 'fa-cloud-rain',         // Moderate drizzle
-        '55': 'fa-cloud-showers-heavy',// Dense drizzle
-        '56': 'fa-icicles',            // Light freezing drizzle
-        '57': 'fa-icicles',            // Dense freezing drizzle
-        '61': 'fa-cloud-rain',         // Light rain
-        '63': 'fa-cloud-rain',         // Moderate rain
-        '65': 'fa-cloud-showers-heavy',// Heavy rain
-        '66': 'fa-snowflake',          // Light freezing rain
-        '67': 'fa-snowflake',          // Heavy freezing rain
-        '71': 'fa-snowflake',          // Light snowfall
-        '73': 'fa-snowflake',          // Moderate snowfall
-        '75': 'fa-snowflake',          // Heavy snowfall
-        '77': 'fa-snowflake',          // Snow grains
-        '80': 'fa-cloud-showers-heavy',// Slight rain showers
-        '81': 'fa-cloud-showers-heavy',// Moderate rain showers
-        '82': 'fa-poo-storm',          // Violent rain showers
-        '85': 'fa-snowflake',          // Light snow showers
-        '86': 'fa-snowflake',          // Heavy snow showers
-        '95': 'fa-bolt',               // Slight/moderate thunderstorm
-        '96': 'fa-bolt',               // Thunderstorm with light hail
-        '99': 'fa-bolt'                // Thunderstorm with heavy hail
+        '0': 'fa-sun',                 
+        '1': 'fa-cloud-sun',           
+        '2': 'fa-cloud-sun',           
+        '3': 'fa-cloud',               
+        '45': 'fa-smog',              
+        '48': 'fa-smog',               
+        '51': 'fa-cloud-rain',         
+        '53': 'fa-cloud-rain',         
+        '55': 'fa-cloud-showers-heavy',
+        '56': 'fa-icicles',            
+        '57': 'fa-icicles',            
+        '61': 'fa-cloud-rain',         
+        '63': 'fa-cloud-rain',         
+        '65': 'fa-cloud-showers-heavy',
+        '66': 'fa-snowflake',          
+        '67': 'fa-snowflake',          
+        '71': 'fa-snowflake',         
+        '73': 'fa-snowflake',          
+        '75': 'fa-snowflake',          
+        '77': 'fa-snowflake',          
+        '80': 'fa-cloud-showers-heavy',
+        '81': 'fa-cloud-showers-heavy',
+        '82': 'fa-poo-storm',          
+        '85': 'fa-snowflake',          
+        '86': 'fa-snowflake',          
+        '95': 'fa-bolt',               
+        '96': 'fa-bolt',               
+        '99': 'fa-bolt'                
     
     };
-    return map[code] || 'fa-question'; // Default to 'question' icon if code not found
+    return map[code] || 'fa-question';//podczas gdy kod nie zostanie oddczytany lub odnaleziony wyświelti pytajnik
 }
 
